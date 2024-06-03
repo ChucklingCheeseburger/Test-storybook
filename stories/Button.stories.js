be2cf0272882311082940dff0c2143d4b59bf68e
ChucklingCheeseburger <aliadcetera@gmail.com>
2024-06-03 15:13:09 -0500

    init again

--- stories/Button.stories.js
+++ stories/Button.stories.js
@@ -20,7 +20,7 @@ export default {
       options: ['small', 'medium', 'large'],
     },
   },
-  // Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
+  //  Use `fn` to spy on the onClick arg, which will appear in the actions panel once invoked: https://storybook.js.org/docs/essentials/actions#action-args
   args: { onClick: fn() },
 };
 
